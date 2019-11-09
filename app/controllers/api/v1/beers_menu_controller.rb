d4b6f8760070f1c9e6f07f42899364d20621d3ee
Lucas Moraes <lucaszmoraes@gmail.com>
2019-11-09 16:28:06 -0300

    passando resposta de service para JSON

--- app/controllers/api/v1/beers_menu_controller.rb
+++ app/controllers/api/v1/beers_menu_controller.rb
@@ -4,9 +4,11 @@ class Api::V1::BeersMenuController < Api::V1::ApiController
   # e calcula a melhor cerveja e a retorna com JSON
   def best_beer_by_temperature
     given_temperature =  beer_by_temperature_params[:temperature].to_i
-    @best_beer_id = BeersMenuService.new(given_temperature).calculates_best_beer_by_temperature
-    @choosen_beer = Beer.find(@best_beer_id)
-    render json: @choosen_beer
+    @best_beer = BeersMenuService.new(given_temperature).calculates_best_beer_by_temperature
+    #@choosen_beer = Beer.find(@best_beer_id)
+    #byebug
+    #@choosen_beer = @best_beer_id.to_json   
+    render json: @best_beer.to_json
   end
 
   private
